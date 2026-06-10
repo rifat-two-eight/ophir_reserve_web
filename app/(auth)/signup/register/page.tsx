f1da30e0be3995b26b1ff90b43761eaa2dc2ab13
rifat-two-eight <rifat28.dev@gmail.com>
2026-06-11 00:39:04 +0600

    suspense fixed

--- app/(auth)/signup/register/page.tsx
+++ app/(auth)/signup/register/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import Link from "next/link";
 import { useSearchParams, useRouter } from "next/navigation";
 
